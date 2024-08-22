3a3fd984a3db64af72a5263877379928cc82c1f8
mamun-bs23 <aminul.mamun@brainstation-23.com>
2024-08-22 18:58:30 +0600

    147 - ErpProductDataModel TaxCategoryName field added.

--- src/Plugins/NopStation.Plugin.B2B.ERPIntegrationCore/Model/ErpProductDataModel.cs
+++ src/Plugins/NopStation.Plugin.B2B.ERPIntegrationCore/Model/ErpProductDataModel.cs
@@ -19,6 +19,7 @@ public class ErpProductDataModel
     public IEnumerable<ErpCategoryDataModel> ProductCategories { get; set; } = new List<ErpCategoryDataModel>();
     public List<KeyValuePair<string, string>> ProductAttributes { get; set; } = new List<KeyValuePair<string, string>>();
     public int TaxCategoryId { get; set; }
+    public string TaxCategoryName { get; set; }
     public string Published { get; set; }
     public string ManufacturerName { get; set; }
     public string ManufacturerCode { get; set; }
