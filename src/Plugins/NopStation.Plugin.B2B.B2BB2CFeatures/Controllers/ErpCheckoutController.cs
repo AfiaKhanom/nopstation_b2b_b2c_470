a3c08afab44f59f97cc93589b92d8eca278d08e4
SaqlainAveiro <anwarul.habib@brainstation-23.com>
2024-08-30 16:15:10 +0600

    2804 - Checkout delivery date parse, culter method removed.

--- src/Plugins/NopStation.Plugin.B2B.B2BB2CFeatures/Controllers/ErpCheckoutController.cs
+++ src/Plugins/NopStation.Plugin.B2B.B2BB2CFeatures/Controllers/ErpCheckoutController.cs
@@ -798,7 +798,7 @@ public class ErpCheckoutController : CheckoutController
                 ModelState.AddModelError("DeliveryDate", "Please provide a valid delivery date");
         }
 
-        if (model.DeliveryDateString == null || !DateTime.TryParseExact(model.DeliveryDateString, "dd/MM/yyyy", new CultureInfo("en-GB"), DateTimeStyles.None, out var selectedDeliveryDate))
+        if (model.DeliveryDateString == null || !DateTime.TryParseExact(model.DeliveryDateString, "dd/MM/yyyy", null, DateTimeStyles.None, out var selectedDeliveryDate))
             ModelState.AddModelError("DeliveryDate", "Please provide a valid delivery date");
 
         //getB2BId for validation
