29050ffa142374822cfb817f460356c8498fb097
Afia Khanom <Afia.Khanom@brainstation-23.com>
2024-12-31 16:41:44 +0600

    ProductListLiveStockDataAsync method implement

--- src/Plugins/NopStation.Plugin.B2B.IQRetailIntegration/IQRetailIntegrationPlugin.cs
+++ src/Plugins/NopStation.Plugin.B2B.IQRetailIntegration/IQRetailIntegrationPlugin.cs
@@ -268,9 +268,9 @@ public class IQRetailIntegrationPlugin : BasePlugin, IAdminMenuPlugin, IErpInteg
         return await _erpIntegrationProductService.GetProductSpecialPricesFromErpAsync(erpRequest);
     }
 
-    public Task ProductListLiveStockDataAsync(ErpAccount erpAccount, IList<Product> products, IProductService productService)
+    public  Task ProductListLiveStockDataAsync(ErpAccount erpAccount, IList<Product> products, IProductService productService)
     {
-        throw new NotImplementedException();
+        return null;
     }
 
     #endregion
