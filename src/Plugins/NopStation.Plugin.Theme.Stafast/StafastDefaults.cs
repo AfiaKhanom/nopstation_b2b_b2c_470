114576c0303b27605dd9b0725a013c8093037871
Mamun.bs23 <aminul.mamun@brainstation-23.com>
2025-08-20 17:26:54 +0600

    default color change

--- src/Plugins/NopStation.Plugin.Theme.Stafast/StafastDefaults.cs
+++ src/Plugins/NopStation.Plugin.Theme.Stafast/StafastDefaults.cs
@@ -2,8 +2,8 @@
 {
     public static class StafastDefaults
     {
-        public static string PrimaryThemeColor => "#6D5DFC";
-        public static string SecondaryThemeColor => "#FA8900";
+        public static string PrimaryThemeColor => "#182986";
+        public static string SecondaryThemeColor => "#e3eaf5";
         public static string FooterBeforeWidgetZone => "footer_before";
         public static string HeaderLinksMiddleWidgetZone => "header_links_middle";
         public static string SocialButtonsAfterWidgetZone => "social_buttons_after";
