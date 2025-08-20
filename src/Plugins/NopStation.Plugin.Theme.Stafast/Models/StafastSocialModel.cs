986dfeac097ebd7249a30c394322c8d334fb43cf
Antor Sarker <antor.sarker@brainstation-23.com>
2025-08-20 14:00:05 +0600

    NopStation.Plugin.Theme.Stafast plugin added

---
+++ src/Plugins/NopStation.Plugin.Theme.Stafast/Models/StafastSocialModel.cs
@@ -0,0 +1,10 @@
+﻿using Nop.Web.Framework.Models;
+
+namespace NopStation.Plugin.Theme.Stafast.Models
+{
+    public partial record StafastSocialModel : BaseNopModel
+    {
+        public string LinkedInLink { get; set; }
+        public string PinterestLink { get; set; }
+    }
+}
---
+++ src/Plugins/NopStation.Plugin.Theme.Stafast/StafastPermissionProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Security;
+using Nop.Services.Security;
+
+namespace NopStation.Plugin.Theme.Stafast
+{
+    public class StafastPermissionProvider : IPermissionProvider
+    {
+        public static readonly PermissionRecord ManageStafast = new PermissionRecord { Name = "Stafast theme. Manage NopStation Stafast theme", SystemName = "ManageNopStationStafast", Category = "NopStation" };
+
+        public virtual IEnumerable<PermissionRecord> GetPermissions()
+        {
+            return new[]
+            {
+                ManageStafast
+            };
+        }
+
+        public HashSet<(string systemRoleName, PermissionRecord[] permissions)> GetDefaultPermissions()
+        {
+            return new HashSet<(string, PermissionRecord[])>
+            {
+                (
+                    NopCustomerDefaults.AdministratorsRoleName,
+                    new[]
+                    {
+                        ManageStafast
+                    }
+                )
+            };
+        }
+    }
+}
